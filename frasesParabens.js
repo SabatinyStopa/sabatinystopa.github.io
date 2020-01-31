f500a92e36c895d90f7972f6c3bd9721b148eafb
Sabatiny Gabriel Stopa <sabatinystopa@gmail.com>
2020-01-31 00:21:57 -0300

    Set realtime database to the project

--- frasesParabens.js
+++ frasesParabens.js
@@ -1,70 +1,31 @@
-let arrayDeFrases = [
-    "felicidades para você, por este dia tão especial que é o seu aniversário.",
-    "parabéns, que possa ter muitos anos de vida, abençoados e felizes, e que estes dias futuros sejam todos de harmonia, paz e desejos realizados.",
-    "que seu coração, esteja sempre em festa, porque você é um ser de luz e especial para mim.",
-    "com todas as minhas forças, eu te desejo um feliz aniversário!",
-    "que seu caminhar seja sempre premiado com a presença de Deus, guiando seus passos e intuindo suas decisões, para que suas conquistas e vitórias, sejam constantes em seus dias.",
-    "foi um ano memorável e inesquecível, e agora é hora de festejar a sua vida. Você está de parabéns. Feliz aniversário e muitas felicidades sempre!",
-    "espero que hoje você celebre seu dia especial junto dos que mais ama, e que o seu coração se aqueça com todo amor que receber. Feliz aniversário!",
-    "a vida é maravilhosa e viver é um privilégio, por isso comemorar aniversário deve ser sempre motivo de alegria e gratidão. Parabéns, muitas felicidades e celebre em grande estilo mais um ano de vida!",
-    "feliz Aniversário! Que a felicidade acompanhe sempre você, não só hoje, mas todos os dias da sua vida.",
-    "feliz aniversário! Você merece o melhor, muita saúde, paz e amor, tudo isso eu lhe desejo assim como muitos anos de vida mais.Parabéns!",
-    "hoje é dia de festa e alegria, pois é o seu aniversário. Parabéns, e celebre com entusiasmo o começo de mais um ano de vida! Felicidades!",
-    "que hoje toda felicidade do mundo se reúna para abraçar seu coração. Tenha um aniversário muito feliz!",
-    "que não faltem sorrisos no seu rosto, samba no coração e paz na mente. Feliz Aniversário!",
-    "feliz Aniversário! Mais um ano que passou e outro que vai começar. Aproveite ao máximo e que nunca lhe falte felicidade, amor, saúde e amizade.",
-    "receba meus votos de um feliz aniversário carregados de carinho. Parabéns, muitas felicidades e mais anos de vida com saúde, paz e amor!",
-    "seu ano foi repleto de suor e determinação, e agora é hora de dar uma relaxada e aproveitar seu aniversário. Parabéns e felicidades sempre!",
-    "que seu aniversário seja repleto de palavras de carinho e abraços sinceros. Parabéns!",
-    "completar mais um ano neste mundo é o maior presente que podemos desejar. Feliz aniversário e que a vida lhe sorria sempre!",
-    "parabéns pelo seu aniversário! Festeje seu dia em grande estilo, e todos os dias celebre a vida e a pessoa maravilhosa que você é.",
-    "parabéns por mais um ano de vida! Sorria, dance, pule de felicidade e celebre seu dia em grande estilo.",
-    "hoje é um dia muito especial, pois você completa mais um aniversário. E você fez por merecer uma enorme comemoração. Parabéns e muitas felicidades!",
-    "não importa quando, o que importa mesmo é lembrar, e ainda que não tenha sido na data exata, eu lembrei do seu aniversário. Parabéns e seja sempre muito feliz!",
-    "que a alegria de viver marque presença nesta linda celebração do seu aniversário. Parabéns, amiga!"
-];
-
-let arrayDeTextosAmiga = [
-    "abra os olhos e sinta o prazer da vida, pois hoje é um dia muito especial. É o seu aniversário! É um dia para refletir sobre o ano que passou. Foram muitos erros e acertos, mas sempre a certeza de viver intensamente cada dia, aproveitando cada momento para aprender e evoluir. Pois, cada dia é mais um passo na longa caminhada da vida. E certamente você como ninguém sabe a pessoa especial que é. Desejo tudo de bom! Que você possa ser feliz e faça os outros felizes, pois não há dádiva maior que a vida e o amor. Por isso, nunca pare a busca pelo crescimento interior e continue sempre a ser a pessoa que você é. Feliz Aniversário!",
-    "parabéns! Que o seu dia seja tão lindo quanto o seu sorriso e lhe ofereça tanta felicidade quanto a sua amizade envia para a minha vida. Que esta nova etapa chegue recheada de muita saúde e novas oportunidades para concretizar os seus sonhos mais desejados. Que a alegria acompanhe você por todos os momentos e que Deus continue guiando todos os seus passos e iluminando cada vez mais os seus pensamentos. Faça com que a sua simpatia possa contagiar ainda mais pessoas, pois você é uma pessoa de muito brilho e a humanidade merece que sua luz seja compartilhada. Nunca se esqueça quanto é especial para a minha vida e de tantas outras pessoas. Feliz Aniversário!",
-    "comemore a maravilha de ser você! Dê uma festa É o seu aniversário. Tire os seus sonhos de Dentro do armário. Repare como o tempo faz mágica Pense nos velhos tempos E vire uma página da vida. Hoje é o seu dia, tome alegria Brinque à vontade O que vale é felicidade, E comemore a maravilha De ser alguém especial. Você é uma pessoa única, Maravilhosa, sem igual! Tenha um Felicíssimo Aniversário! Com todo meu carinho Felicidades, sucesso, Saúde e muito amor, Hoje e sempre!",
-    "tantas pessoas já passaram pela minha vida ao longo desses anos todos, mas poucas delas permaneceram por muito tempo em minha memória. Algumas desapareceram tão rápido quanto apareceram, mas outras como você, tenho certeza que continuarão eternamente em minha vida. Sua amizade é muito especial para mim, cada segredo confessado, cada abraço apertado e cada conselho trocado foram essenciais para a construção do nosso relacionamento. Todos os momentos que vivi na sua companhia foram extremamente válidos, você é uma pessoa fantástica, que espero que esteja sempre ao meu lado. Curta o dia de hoje com ainda mais intensidade que os outros dias, pois a celebração da vida é uma data importantíssima que deve ser festejada com muita alegria! Espero que a felicidade esteja sempre transbordando na sua vida, que nunca lhe falte saúde e muitas outras conquistas. Feliz aniversário! Tenha um excelente dia!",
-    "para este dia tão especial queria poder me expressar com as palavras mais bonitas do universo e com elas dizer quanto você é especial. Mas a verdade é que não existem palavras suficientemente perfeitas e capazes de demonstrar todas suas qualidades. Feliz aniversário, amiga! Você é linda, por dentro e por fora, sincera, alegre e bondosa. Você é uma amiga como não há igual. Estas palavras foram escolhidas pelo meu carinho e toda a amizade que sinto por você. O mesmo carinho que me inspira a desejar-lhe toda felicidade do mundo, hoje e sempre. Continue sendo quem é, pois eu amo você desse jeito, mas saiba que se um dia mudar, eu continuarei a amar você. Parabéns, minha amiga especial!",
-    "nada como chegar nesta data e ver tudo que passamos, o quanto somos queridos neste mundo que vivemos. É muito bom saber, que os anos vão, a idade chega, e você sempre continua o mesmo, sempre com o mesmo sorriso, sempre com a mesma alegria de viver. Parabéns e muitas felicidades a você, que com esse seu jeitinho, consegue transmitir toda a alegria, para fazer uma pessoa feliz. Que neste aniversário, você consiga descobrir muito mais ideais, do que aqueles já conseguidos, e fazer disso uma lição de vida. Desejo a você tudo de ótimo que a vida pode lhe oferecer, porque você merece! UM SUPER FELIZ ANIVERSÁRIO! De uma pessoa que gosta muito de você!",
-    "amizade não é algo que está escrito em um papel, pois o papel pode ser rasgado. Também não é algo que pode ser escrito em uma pedra, pois mesmo uma pedra pode quebrar. Mas está escrito no coração de uma pessoa, e ela fica lá para sempre. Desejo um feliz aniversário e muitas bênçãos em sua vida, minha amiga!",
-    "procurei uma amiga que me diga sempre a verdade, que não camufle os meus defeitos, que não despreze as minhas lágrimas! Uma amiga cuja presença traga alegria, cujo silêncio transmita paz cuja escuta inspire confiança, cuja lembrança infunda coragem. Uma amiga à qual eu possa dizer: desculpa! Uma, duas, três vezes. Uma amiga que não seja nem mestre, nem discípula, mas uma companheira, com a qual eu possa caminhar rumo ao infinito em qualquer momento. Uma amiga que conserve a sua intimidade sem esconder o seu pranto. Uma amiga que ao amanhecer não me diga bom dia, mas abra o seu coração com um amável sorriso. Uma amiga que creia na amizade e a viva como uma audaz conquista de liberdade. Cuja amizade seja óleo doce, suave e perfumado, extraído do fruto amargo de uma árvore espinhosa. Uma amiga que não se preocupe em dar ou receber, mas que seja capaz de compartilhar. Uma amiga simples, sincera, natural, capaz de chorar, mas sobretudo de sorrir. Uma amiga que seja um reflexo da bondade de Deus. Procurei uma amiga especial e encontrei você! Feliz Aniversário!",
-    "hoje é seu dia e tanto eu como o céu estamos felizes por esta data de alegria. Quero nesta mensagem expressar o carinho e o orgulho que tenho por você. Que o seu exemplo de vida reflita em nosso dia a dia e que cada um de nós possa aprender com a sua bondade. Parabéns por mais este ano, por essa alegria de viver! Espero que a cada ano você adquira novas forças e que continue firme e vencendo sempre. Deus está do seu lado e terá sempre uma bênção para lhe oferecer. Obrigada por ser tão especial, por ter sempre uma palavra amiga para me dizer quando meu coração está cansado de sofrer e chorar. Que os anjos acampem sempre ao seu redor, iluminando sua vida. Parabéns e muitos anos de saúde, felicidade e fé! Você merece e eu torço por isso. Parabéns, seja feliz em todos os anos da sua vida!",
-    "amiga linda, hoje é o seu dia, um dia mais que especial e feliz na vida de todo mundo que tem o prazer de conviver com você. Eu nem precisaria lhe desejar um feliz aniversário, porque para pessoas como você o dia em que se completa anos não poderia ser diferente, é sempre um dia de festa! Hoje, você faz aniversário e espero que o dia já lhe felicite com um belo sorriso do sol entrando pela sua janela, para que você possa sentir a graciosidade da vida e como ela se revela feliz nas pequenas coisas. Desejo que durante todo o seu dia, muitos amigos e familiares se lembrem de você, e que lhe digam palavras bonitas, dizendo como você é importante e especial, e como lhe desejam a felicidade. E espero que à noite, você possa estar cercada das pessoas que ama, dos amigos e entes mais próximos e queridos, e receba muitos abraços e beijinhos, e carinhos sem fim. Desejo também que a vida lhe dê todos os presentes que você quiser, e lhe dê muita força e saúde para lutar por aquilo que acredita que lhe fará feliz. Parabéns e felicidades hoje e sempre",
-    "parabéns por esta data especial, amiga! Que o tempo seja sempre o seu melhor parceiro, trazendo serenidade, equilíbrio e sabedoria – que lhe darão a receita ideal de como viver a vida, aproveitando o melhor que ela tem a oferecer. Que as conquistas do passado lhe tragam à lembrança, não só alegrias, mas também a força e o entusiasmo para superar eventuais obstáculos e implementar os mais importantes projetos para o futuro. Muita saúde, amor, paz, fé, esperança, alegrias e prosperidade! Um sincero abraço e votos de muitas felicidades da tua amiga que, apesar de distante, nunca te esquece!",
-    "no seu aniversário, levante o rosto para o sol e siga sua estrela! Vá buscar seus sonhos além do arco-íris, por mais impossíveis que pareçam. Embale no peito as maiores esperanças e ouça sempre seu coração. Acredite sempre e siga o seu caminho, pois o amanhã nasce do que você sonha hoje. Que neste aniversário, e a cada dia que vier você esteja sempre mais perto dos seus sonhos! Feliz Aniversário!",
-    "querida amiga, a vida é um presente do Pai e cada aniversário é um novo começo. Desejo que venha de Deus a mais completa renovação, que ela flua interiormente e contagie a todos que tenham o privilégio de estar ao seu redor. Faça com que cada dia seja encarado com esperança de modo que as sombras do ontem não sejam levadas para a luz do amanhã. Um olhar amigo alegra o coração, uma boa notícia nos faz sentir bem. Que a luz do Pai ilumine sua vida, guie seus passos e alegre seu caminho em toda sua existência. Não só hoje, mas sempre. Feliz aniversário, minha amiga, seja imensamente feliz!",
-    "hoje o dia amanheceu mais cedo, para lhe dar os parabéns. Hoje o sol está mais brilhante, para iluminar ainda mais o seu grande dia. Hoje as nuvens se esconderam, para preparar uma surpresa para você. Hoje a tarde virá mais devagar, para dar tempo de todos lhe desejarem felicidades. Hoje o pôr do sol será mais lento, para a lua vir com as estrelas e festejarem com você. E quando chegar a noite não esqueça de agradecer o seu dia, assim como eu agradeci por ter conhecido você. Te Amo!"
-];
-
 function CreateCongratsMessage(){
-    let NomeDoCara = document.getElementById("nomeDoDesgracado").value;
-    GenerateText(NomeDoCara);
+    var Name = document.getElementById("nomeDoDesgracado").value;
+    GetDatabaseValue(ChooseDatabaseChild(TakeSelectedField()), Name);
 }
 
-function GenerateText(NomeDoCara){
-    document.getElementById("message").innerHTML = NomeDoCara.concat(", ", ChooseOneArray(TakeSelectedField())[RandomNumber(ChooseOneArray(TakeSelectedField()))]);        
+function RandomNumber(number){
+    return Math.floor(Math.random() * number);
 }
 
-function RandomNumber(array){
-    let numero = Math.floor(Math.random() * array.length);
-
-    return numero;
-}
-
-function ChooseOneArray(FieldValue){
+function ChooseDatabaseChild(FieldValue){
     if(FieldValue == "frases")
-        return arrayDeFrases;
+        return "frases_comuns";
     else
-        return arrayDeTextosAmiga
+        return "frases_amiga";
 }
 
 function TakeSelectedField(){
-    let FieldValue = document.getElementById("opcoes").value;
-    
+    var FieldValue = document.getElementById("opcoes").value;
     return FieldValue;
+}
+
+function GetDatabaseValue(Text, Name){
+    var database = firebase.database();
+    var object = database.ref(Text).once('value');
+    object.then(function(snapshot){
+        var position = RandomNumber(snapshot.numChildren()).toString();
+        var result = Name.concat(", ", snapshot.child(position).val().toString()); 
+        document.getElementById("message").innerHTML = result;
+    });
+       
 }
